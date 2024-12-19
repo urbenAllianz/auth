d2788380a633d57376901624398f7112647c9f0f
TechShin0bi <urben.fotso@allianz.com>
2024-12-20 00:16:29 +0100

    phone nmber added to user list

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate , logout
-from .forms import CustomUserCreationForm
-import logging
+from django.contrib.auth import login, authenticate, logout
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .forms import UserProfileForm
-from . models import CustomUser as User
+from .forms import CustomUserCreationForm, UserProfileForm
+from .models import CustomUser as User
+import logging
 
 @login_required
 def user_profile(request):
+    """
+    View to handle user profile updates.
+    """
     user = request.user  # Get the current logged-in user
 
     if request.method == 'POST':
@@ -25,33 +25,27 @@ def user_profile(request):
 
     return render(request, 'accounts/user_profile.html', {'form': form, 'user': user})
 
-
-
-
 def user_list(request):
-    # Get the search term from the request (if any)
+    """
+    View to display a list of users with pagination and search functionality.
+    """
     search_query = request.GET.get('search', '')
-    
-    # Filter users based on search query (you can adjust the fields you want to search)
     users = User.objects.filter(username__icontains=search_query).order_by('-date_joined')
-    
-    # Set up pagination
     paginator = Paginator(users, 10)  # Show 10 users per page
-    page_number = request.GET.get('page')  # Get the current page number from the URL
+    page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
     return render(request, 'accounts/user_list.html', {'page_obj': page_obj, 'search_query': search_query})
 
-
-
 logger = logging.getLogger(__name__)
 
 def login_view(request):
+    """
+    View to handle user login.
+    """
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        # Authenticate using Django's default user model
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
@@ -62,8 +56,10 @@ def login_view(request):
 
     return render(request, 'accounts/login.html')
 
-
 def register_view(request):
+    """
+    View to handle user registration.
+    """
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -72,7 +68,6 @@ def register_view(request):
             messages.success(request, "Registration successful!")
             return redirect('/')  # Redirect to home or any other page
         else:
-            print(form.errors)
             # Collect and display all form field errors
             for field, errors in form.errors.items():
                 for error in errors:
@@ -82,8 +77,9 @@ def register_view(request):
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
-
-
 def logout_view(request):
+    """
+    View to handle user logout.
+    """
     logout(request)
     return redirect('login')
